d66baa5a03527c351ad0b4f44a8454aab99d65ec
dhrbtjr0331 <dhrbtjr331@MacBookAir.attlocal.net>
2025-10-03 14:10:31 -0700

    setup initial project: spring boot + react + postgreSQL

---
+++ partyapp/src/main/java/com/carrick/partyapp/PartyappApplication.java
@@ -0,0 +1,13 @@
+package com.carrick.partyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyappApplication.class, args);
+	}
+
+}
